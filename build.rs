23c441a88be5881c1e815ffe4a24253c64231e41
Aaron Power <theaaronepower@gmail.com>
2018-04-02 18:46:23 +0100

    fixed bootstrap

--- build.rs
+++ build.rs
@@ -1,3 +1,4 @@
+#[cfg(not(feature="no-test"))]
 extern crate chelone;
 extern crate url;
 
@@ -36,7 +37,10 @@ urls!{
     qt:    "http://www.w3.org/2001/sw/DataAccess/tests/test-query#"
 }
 
+#[cfg(feature="no-test")]
+fn main() {}
 
+#[cfg(not(feature="no-test"))]
 fn main() {
     let out_dir = env::var_os("OUT_DIR").expect("can't get OUT_DIR");
     let base_url = {
